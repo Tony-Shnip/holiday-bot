7a367f5c1c2e45b0576224a48a9503c11aadbef6
Anton Shnip <toni.mr.anton@mail.ru>
2023-05-12 16:55:33 +0300

    Init project

---
+++ index.js
@@ -0,0 +1,48 @@
+const { TelegramClient } = require("telegram");
+const { StringSession } = require("telegram/sessions");
+const cron = require('node-cron');
+const request = require('request');
+
+require('dotenv').config();
+
+const apiId = process.env.API_ID;
+const apiHash = process.env.API_HASH
+const stringSession = new StringSession(process.env.LOG_STRING);
+
+const months = {
+  0: "yanvar",
+  1: "fevral",
+  2: "mart",
+  3: "aprel",
+  4: "may",
+  5: "iyun",
+  6: "iyul",
+  7: "avgust",
+  8: "sentyabr",
+  9: "oktyabr",
+  10: "noyabr",
+  11: "dekabr"
+};
+
+(async () => {
+  console.log("Loading interactive example...");
+  const client = new TelegramClient(stringSession, apiId, apiHash, {
+    connectionRetries: 5,
+  });
+
+  await client.connect();
+
+  console.log("You should now be connected.");
+
+  cron.schedule('0 12 * * *', async () => {
+    const date = new Date();
+
+    request(process.env.HOLIDAY_API + months[date.getUTCMonth()] + '/' + date.getDate(), async (err, res, body) => {
+      if (err) throw err;
+      
+      const holidays = /празднуем: (.*?)и ещё/.exec(body)[1].split(', ');
+
+      await client.sendMessage(process.env.PHONE, { message: "Солнышко, улыбнись! Ведь сегодня мы с тобой отмечаем " + holidays[0] + '. ' + 'С ПРАЗДНИКОМ!!!' });
+    });
+  });
+})();
